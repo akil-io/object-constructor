b16a6170231fe35a3a368d07b49dbb078f6afe69
fenix <fenixdeveloper@gmail.com>
2022-01-26 11:29:50 +0300

    refactor for use with static defined models

---
+++ examples/test.js
@@ -0,0 +1,59 @@
+const { Schema, deepMerge, StaticSchema } = require('../');
+const _ = require("lodash");
+
+class Model {
+    static fields = {
+        id: null,
+        uid: null,
+        __owner: null,
+        __access: {},
+        __version: 0,
+        __created: null,
+        __changed: null,
+        __deleted: null
+    };
+    static views = {
+        default: ['uid']
+    };
+
+    constructor(fields = {}) {
+        this.reset(fields);
+    }
+
+    static schema (name) {
+        return Schema.deep(this, name);
+    }
+
+    reset(fields) {
+        let schema = this.constructor.schema('fields');
+        for (let key in schema) {
+            if (fields[key] !== undefined) this[key] = fields[key];
+            else this[key] = schema[key];
+        }
+    }
+
+    view(name = 'default') {
+        let views = this.constructor.schema('views');
+        return _.pick(this, views[name]);
+    }
+}
+
+class User extends Model {
+    static fields = {
+        name: ""
+    };
+    static views = {
+        default: ['name']
+    };
+
+    constructor(fields) {
+        super(fields);
+    }
+}
+
+let u = new User();
+console.log(`user: `, u);
+console.log(`user default: `, u.view());
+
+
+
--- index.js
+++ index.js
@@ -1,117 +1,22 @@
-const _ = require('lodash');
+const { Schema } = require('./lib/Schema');
+const { deepMerge } = require('./lib/deepMerge');
 
-function Schema(name, builders) {
-	Schema[name] = builders;
+class StaticSchema {
+    static schema (name) {
+        return Schema.deep(this, name);
+    }
+
+    reset(fields) {
+        let schema = this.constructor.schema('fields');
+        for (let key in schema) {
+            if (fields[key] !== undefined) this[key] = fields[key];
+            else this[key] = schema[key];
+        }
+    }
 }
-
-Schema.builder = function (name, build, settings) {
-	// target: property, method
-	// context: internal, external
-	return {
-		name, build, ...settings
-	};
-}
-
-Schema.group = function (name, builders) {
-	return Schema.builder(name, 'group', {
-		builders
-	});
-}
-Schema._boundFunc = func => context => (func.prototype === undefined && func.constructor.name != 'AsyncFunction') ? function () { return func(context, ...arguments); } : func.bind(context);
-
-Schema._proxy = function (get, set) {
-	let settings = {};
-	if (get) settings.get = Schema._boundFunc(get);
-	if (set) settings.set = Schema._boundFunc(set);
-	return settings;
-}
-
-Schema.property = function (name, writable, enumerable, value) {
-	let settings = { enumerable };
-	if (_.isObject(value) && (value.get || value.set)) {
-		Object.assign(settings, value);
-	} else {
-		settings.value = value;
-		settings.writable = writable;
-	}
-	return Schema.builder(name, "property", settings);
-}
-Schema.public = function (name, value) {
-	return Schema.property(name, true, true, value);
-}
-Schema.protected = function (name, value) {
-	return Schema.property(name, true, false, value);
-}
-Schema.readonly = function (name, value) {
-	return Schema.property(name, false, true, value);
-}
-
-Schema.method = function (name, func) {
-	return Schema.builder(name, "method", {
-		value: Schema._boundFunc(func),
-		writable: false,
-		enumerable: false
-	});
-}
-
-Schema.flatten = function (builders = [], schemaName = 'Self', result = {}) {
-	result[schemaName] = [];
-	for (let item of builders) {
-		switch (item.build) {
-			case "group":
-				Schema.flatten(item.builders, item.name, result);
-				break;
-			default:
-				result[item.name] = item;
-				result[schemaName].push(item.name);
-		}
-	}
-
-	return result;
-}
-
-Schema.apply = function (context, builders, initial = {}) {
-	let flattened = Schema.flatten(builders);
-	let properties = {};
-	let groups = [];
-
-	for (let field in flattened) {
-		if (_.isArray(flattened[field])) {
-			groups.push(field);
-			continue;
-		}
-		let { build: type, enumerable, writable, value, get, set } = flattened[field];
-
-		properties[field] = {
-			enumerable
-		};
-
-		if (get !== undefined || set !== undefined) {
-			if (get !== undefined) {
-				properties[field].get = get(context);
-			}
-			if (set !== undefined) {
-				properties[field].set = set(context);
-			}
-		} else {
-			properties[field].writable = writable;
-			if (type == "method" && _.isFunction(value)) {
-				properties[field].value = value(context);
-			} else {
-				properties[field].value = (initial[field] !== undefined) ? initial[field] : value;
-			}
-		}
-	}
-
-	Object.defineProperties(context, properties);
-
-	return context;
-}
-
-Schema.lock = function (context) {
-	Object.seal(context);
-};
 
 module.exports = {
-	Schema
+    Schema,
+    deepMerge,
+    StaticSchema
 };
